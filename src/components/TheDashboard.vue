4b526fecd880acce1f4275cd0051675b9dd13a7a
Rianne Maceda <157263717+ri4nne@users.noreply.github.com>
2025-06-19 09:48:58 +0800

    setup dashboard page

--- src/components/TheDashboard.vue
+++ src/components/TheDashboard.vue
@@ -1,5 +1,15 @@
 <template>
-  <div></div>
+  <div>
+    <el-card>
+      <template #header>
+        <div class="card-header">
+          <span>Card name</span>
+        </div>
+      </template>
+      <p v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</p>
+      <template #footer>Footer content</template>
+    </el-card>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
--- src/views/TheDashboardView.vue
+++ src/views/TheDashboardView.vue
@@ -1,7 +1,22 @@
 <template>
-  <div></div>
+  <div class="dashboard">
+    <h1>Welcome to the Dashboard</h1>
+    <p>This is the dashboard page.</p>
+  </div>
+  <TheDashboard />
 </template>
 
-<script setup lang="ts"></script>
+<script lang="ts" setup>
+import TheDashboard from '/src/components/TheDashboard.vue'
+</script>
 
-<style scoped></style>
+<style scoped>
+.dashboard {
+  padding: 20px;
+  background-color: rgba(255, 255, 255, 0.8);
+  border-radius: 8px;
+  max-width: 800px;
+  margin: 20px auto;
+  text-align: center;
+}
+</style>
