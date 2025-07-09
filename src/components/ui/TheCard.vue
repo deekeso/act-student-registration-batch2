8abf6b1e04e3bb865b96a3af86f358807dc5f992
FCL00 <flagahit00@gmail.com>
2025-07-09 10:23:28 +0800

    fix: remove drawer and student form

--- src/components/ui/TheCard.vue
+++ src/components/ui/TheCard.vue
@@ -3,9 +3,9 @@
     <div class="card-header">
       <div class="card-group">
         <el-icon><User /></el-icon>
-        <h5 class="fullname">{{ fullname }}</h5>
+        <h5 class="fullname">{{ fullname.length > 30 ?  `${fullname.slice(0, 30)}...` : fullname }}</h5>
       </div>
-      <button color="#2148c0" @click="drawer = true">
+      <button color="#2148c0" @click="openDrawer">
         <el-icon color="white">
           <Edit />
         </el-icon>
@@ -26,56 +26,40 @@
       </div>
       <div class="icon">
         <el-icon><MapLocation /></el-icon>
-        <p>Address: <span>{{ address }}</span></p>
+        <p>Address: <span class="ellipsis">{{ address.length > 50 ? `${address.slice(0, 50)}...` :  address }}</span></p>
       </div>
     </div>
   </el-card>
-
-  <the-drawer v-model:visible="drawer">
-    <student-form
-      :firstname="props.firstname"
-      :lastname="props.lastname"
-      :middlename="props.middlename"
-      :age="props.age"
-      :birthdate="birthdate"
-      :address="address"
-      :course="props.course"
-      :id="props.id"
-      @on-drawer-close="onDrawerClose"
-    ></student-form>
-  </the-drawer>
 </template>
 
 <script lang="ts" setup>
-import { defineProps, computed, ref } from 'vue'
+import { defineProps, computed, defineEmits } from 'vue'
 import { ElCard } from 'element-plus'
 import type { Users } from '@/types'
 import { Edit, Calendar, School, MapLocation, User, IceTea } from '@element-plus/icons-vue'
-import { TheDrawer } from '@/components/ui'
-import { StudentForm } from '@/components'
 import { capitalizeEachWord } from '@/utils/capitalize'
-// to toggle the drawer
-const drawer = ref(false)
+
+// toggle drawer
+const emit = defineEmits(['open-drawer'])
+
 
 // display the fullname
 const fullname = computed(() => {
   if(props.middlename){
-    return `${capitalizeEachWord(props.firstname)} ${props.middlename?.charAt(0).toUpperCase()}. ${capitalizeEachWord(props.lastname)} `
+    return `${capitalizeEachWord(props.firstname ?? '')} ${props.middlename?.charAt(0).toUpperCase()}. ${capitalizeEachWord(props.lastname ?? '')} `
   } else {
-    return `${capitalizeEachWord(props.firstname)} ${capitalizeEachWord(props.lastname)} `
+    return `${capitalizeEachWord(props.firstname ?? '')} ${capitalizeEachWord(props.lastname ?? '')} `
   }
 })
 
 // display address 
-const address = computed(() => `${capitalizeEachWord(props.address)}`)
+const address = computed(() => `${capitalizeEachWord(props.address ?? '')}`)
 // define props
 const props = defineProps<Users>()
 
 
-const onDrawerClose = () => {
-  drawer.value = false;
-  console.log('on drawer close', )
-
+const openDrawer = () => {
+  emit('open-drawer', props)
 }
 </script>
 
@@ -85,6 +69,7 @@ const onDrawerClose = () => {
   white-space: nowrap;
   margin-left: 12px;
 }
+
 p {
   font-size: small;
   font-weight: bold;
