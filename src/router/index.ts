85c571d23b004e0b720ef663c13df5c068905749
FCL00 <flagahit00@gmail.com>
2025-07-09 10:57:08 +0800

    fix: add route guards and private routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,19 +4,68 @@ import LoginPage from '@/views/LoginPage.vue'
 import DashBoard from '@/views/DashBoard.vue'
 import ForgotPasswordPage from '@/views/ForgotPasswordPage.vue'
 import NotFound from '@/views/NotFound.vue'
+import { useAuth } from '@/composables/useAuth'
+import { ElMessage } from 'element-plus'
+const routes = [
+  {
+    path: '/',
+    component: LoginPage,
+    meta: { middleware: 'guest' },
+  },
+  {
+    path: '/login',
+    component: LoginPage,
+    meta: { middleware: 'guest' },
+  },
+  {
+    path: '/dashboard',
+    component: DashBoard,
+    meta: { middleware: 'auth' },
+  },
+  {
+    path: '/forgot-password',
+    component: ForgotPasswordPage,
+    meta: { middleware: 'guest' },
+  },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
+]
 
 // Create and configure router instance
 const router = createRouter({
   // Use HTML5 history mode (no # in URLs)
   history: createWebHistory(import.meta.env.BASE_URL),
   // Define application routes
-  routes: [
-    { path: '/', name: 'index', component: LoginPage },
-    { path: '/login', name: 'login', component: LoginPage },
-    { path: '/dashboard', name: 'dashboard', component: DashBoard },
-    { path: '/forgot-password', name: 'forgot-password', component: ForgotPasswordPage },
-    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound }, // Catch-all route for undefined paths (404 handler)
-  ],
+  routes
 })
+
+
+router.beforeEach((to, from, next) => {
+  const { isAuthenticated } = useAuth()
+  const middleware = to.meta.middleware
+
+  if (!middleware) {
+    return next()
+  }
+
+  if (middleware === 'auth') {
+    if (isAuthenticated()) {
+      return next()
+    } else {
+      ElMessage({ type: 'error', message: 'You need logged in before accessing this page', showClose: true} )
+      return next('/')
+    }
+  }
+
+  if (middleware === 'guest') {
+    if (isAuthenticated()) {
+      return next('/dashboard')
+    } else {
+      return next()
+    }
+  }
+
+  next()
+})
+
 
 export default router
